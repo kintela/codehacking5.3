d58ea8afcd9bbaa3b46bddc592783b3ec7ac6413
kintela <roberto.quintela@gmail.com>
2017-01-24 14:20:03 +0100

    editorwysywyg

--- config/app.php
+++ config/app.php
@@ -154,6 +154,8 @@ return [
         /*
          * Application Service Providers...
          */
+        Unisharp\Laravelfilemanager\LaravelFilemanagerServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
@@ -206,6 +208,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
